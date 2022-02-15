5f4fef79dabe76837b6952990ade9f84975519ac
Artur Frącek <arturfracek7@gmail.com>
2022-02-15 18:09:52 +0100

    Created Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,52 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import api from "@/api";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: () => ({
+    votes: [],
+    vote: null
+  }),
+
+  mutations: {
+    SET_VOTES(state, votes) {
+      state.votes = votes;
+    },
+    ADD_VOTE(state, vote) {
+      state.votes.push(vote);
+    },
+    SET_VOTE(state, vote) {
+      state.vote = vote;
+    },
+  },
+
+  actions: {
+    async fetchAllVotes({ commit }) {
+      const votes = (await api.get("/votes")).data;
+      commit("SET_VOTES", votes);
+    },
+    async fetchVoteById({ commit }, vote_id) {
+      const vote = (await api.get(`/votes/${vote_id}`)).data;
+      commit("SET_VOTE", vote);
+    },
+    async deleteVote({ dispatch }, vote_id ) {
+      await api.delete(`/votes/${vote_id}`);
+      await dispatch("fetchAllVotes");
+    },
+    async createVote({ dispatch }, newVote) {
+      await api.post("/votes", newVote);
+      await dispatch("fetchAllVotes");
+    },
+  },
+
+  getters: {
+    votes(state) {
+      return state.votes;
+    },
+    vote(state) {
+      return state.vote
+    }
+  },
 });
